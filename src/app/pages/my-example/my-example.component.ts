2ddef372bf85d8624ce5c0cecd49585b98ec7c0c
Alexis Cepeda <alexkiller0408@hotmail.com>
2025-09-18 23:55:05 -0500

    usando angular material

--- src/app/pages/my-example/my-example.component.ts
+++ src/app/pages/my-example/my-example.component.ts
@@ -1,11 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, signal, ViewChild, ElementRef } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
+import { MatIconModule } from '@angular/material/icon';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-my-example',
-  imports: [],
+  imports: [NgIf, MatIconModule],
   templateUrl: './my-example.component.html',
   styleUrl: './my-example.component.css'
 })
 export class MyExampleComponent {
+  imageName = signal('');
+  fileSize = signal(0);
+  uploadProgress = signal(0);
+  imagePreview = signal('');
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+  selectedFile: File | null = null;
+  uploadSuccess: boolean = false;
+  uploadError: boolean = false;
+
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  // Handler for file input change
+  onFileChange(event: any): void {
+    const file = event.target.files[0] as File | null;
+    this.uploadFile(file);
+  }
+
+  // Handler for file drop
+  onFileDrop(event: DragEvent): void {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0] as File | null;
+    this.uploadFile(file);
+  }
+
+  // Prevent default dragover behavior
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  // Method to handle file upload
+  uploadFile(file: File | null): void {
+    if (file && file.type.startsWith('image/')) {
+      this.selectedFile = file;
+      this.fileSize.set(Math.round(file.size / 1024)); // Set file size in KB
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.imagePreview.set(e.target?.result as string); // Set image preview URL
+      };
+      reader.readAsDataURL(file);
+
+      this.uploadSuccess = true;
+      this.uploadError = false;
+      this.imageName.set(file.name); // Set image name
+    } else {
+      this.uploadSuccess = false;
+      this.uploadError = true;
+      this.snackBar.open('Only image files are supported!', 'Close', {
+        duration: 3000,
+        panelClass: 'error',
+      });
+    }
+  }
+
+  // Method to remove the uploaded image
+  removeImage(): void {
+    this.selectedFile = null;
+    this.imageName.set('');
+    this.fileSize.set(0);
+    this.imagePreview.set('');
+    this.uploadSuccess = false;
+    this.uploadError = false;
+    this.uploadProgress.set(0);
+  }
 
 }
